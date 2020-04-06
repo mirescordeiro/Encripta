d50bcacc701e7906521293903a1f0977a27bfb1c
Tamires Cordeiro <tamirescordeiro@Mac-mini-de-Tamires.local>
2020-04-06 10:59:12 -0300

    JS e HTML

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,7 @@
 const cipher = {
-  // ...
+  encode = 
+  decode =
+
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+// Palavra
+
+let word = document.getElementById("word");
+
+// Deslocamento
+
+let offset = document.getElementById("offset");
+
+
+// Botão Cifrar
+
+let buttonCipher = document.getElementById("buttonCipher");
+
+buttonCipher.addEventListener("click", cipher.encode);
+
+
+// Botão Decifrar
+
+let buttonDecipher = document.getElementById("buttonDecipher");
+
+buttonDecipher.addEventListener("click", cipher.decode);
+
+
+// Resultado
+
+let outcome = cipher;
+
+document.getElementById("displayOutcome").innerHTML = outcome;
