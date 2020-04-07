ebaccd56f5932a778e15adbc0db6b8927233d308
Tamires Cordeiro <tamirescordeiro@Mac-mini-de-Tamires.local>
2020-04-07 01:34:52 -0300

    Cipher

--- src/cipher.js
+++ src/cipher.js
@@ -3,17 +3,27 @@ const cipher = {
   // Cifrar
 
   encode: function encode(word, offset) {
-    const encipher = word + offset;
+    let encipher = "";
+    let letters = "";
+    for (let l = 0; l < word.length; l++) {
+      letters = ((word[l].charCodeAt() - 65 + offset) % 26) + 65;
+      encipher += String.fromCharCode(letters);
+    }
     return encipher;
   } 
   
   // Decifrar
 
-  decode: function decode(word, offset) {
-    const decipher = word  offset;
-    return decipher;
-  }
+  //decode: function decode(word, offset) {
+  //  const decipher = word  offset;
+  //  return decipher;
+  //}
 
 };
 
 export default cipher;
+
+
+// ((codigoDaLetraASC - cod1aLetra + desloc) % tamDoAlfabeto) + cod1aLetra
+
+// codigoASC => codigo0a25 => desloco => giro => codigoASC 
