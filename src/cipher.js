7cfd721c9d8d538442aee31e839d3147e3a4d3f7
Tamires Cordeiro <tamirescordeiro@Mac-mini-de-Tamires.local>
2020-08-23 17:09:57 -0300

    limpar comentários e aplicar estilos refatorados dos botões

--- src/cipher.js
+++ src/cipher.js
@@ -1,24 +1,18 @@
 const cipher = {
   
-  // CIFRAR
   encode: function (offset, message) {
     let encipher = "";
-    // Validar parâmetros
     if (offset !== 0 && message !== ""){
-      // Loop da string
       for (let l = 0; l < message.length; l++) {
         let number = Number(offset);
         let ascii = message.charCodeAt(l);
         let letters = [];
-        // Maiúsculas
         if (65 <= ascii && ascii <= 90){
           letters = ((ascii - 65 + number) % 26) + 65;
         }
-        // Minúsculas
         else if (97 <= ascii && ascii <= 122){
           letters = ((ascii - 97 + number) % 26) + 97;
         }
-        // Caracteres especiais
         else {
           letters = ascii;
         }
@@ -30,26 +24,19 @@ const cipher = {
     }
   }, 
 
-
-  // DECIFRAR
   decode: function (offset, message) {
     let decipher = "";
-    // Validar parâmetros
     if (offset !== 0 && message !== ""){
-      // Loop da string
       for (let l = 0; l < message.length; l++) {
         let number = Number(offset);
         let ascii = message.charCodeAt(l);
         let letters = [];
-        // Maiúsculas
         if (65 <= ascii && ascii <= 90){
           letters = ((ascii - 90 - number) % 26) + 90;
         }
-        // Minúsculas
         else if (97 <= ascii && ascii <= 122){
           letters = ((ascii - 122 - number) % 26) + 122;
         }
-        // Caracteres especiais
         else {
           letters = ascii;
         }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,18 @@
 import cipher from './cipher.js';
 
-// Mensagem
 let message = document.getElementById("message");
 
-// Deslocamento
 let offset = document.getElementById("offset");
 
-// Botão Reset
 const buttonReset = document.getElementById("buttonReset");
 buttonReset.addEventListener("click", function(){location.reload()});
 
-// Botão Decifrar
 const buttonDecipher = document.getElementById("buttonDecipher");
 
 function decipher() {
     let outcomeDecipher = cipher.decode(offset.value, message.value);
     
     if (offset.value === "") {
-        // Mensagem de alerta
         return alert ("Parece que você esqueceu de preencher o deslocamento.");
     } else {
         document.getElementById("displayOutcome").innerHTML = outcomeDecipher;
